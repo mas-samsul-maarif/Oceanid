aca4dd31ea9b06a9d21ee7a44c070fc3561022f0
Rian Ananda <72483235+RianAnanda@users.noreply.github.com>
2021-06-02 21:48:40 +0700

    Database Update 1.0

--- application/views/form_isi.php
+++ application/views/form_isi.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style.css">
     <title>Document</title>
 </head>
 <body>
     <br>
     <center>
-    <div class='card' style="width: 500px; height: 270px;"><br>
+    <div class='card' style="width: 500px; height: 270px;">
     <form action="<?php echo base_url()."index.php/hal_utama/tambah_data"?>" method="POST">
     <table class='table1'>
         <tr><td>Nama Umum:</td><td> <input style="width: 350px;" type="text" name="umum"> </td>
         <tr><td>Nama Ilmiah: </td><td><input style="width: 350px;" type="text" name="ilmiah"></td>
-        <tr><td>kedalaman: </td><td><input style="width: 350px;" type="text" name="kedalaman"></td>
-        <tr><td>keterangan: </td><td><input style="width: 350px;" type="text" name="keterangan"></td>
+        <tr><td>Kedalaman: </td><td><input style="width: 350px;" type="text" name="kedalaman"></td>
+        <tr><td>Keterangan: </td><td><input style="width: 350px;" type="text" name="keterangan"></td>
     </table><br>
         <input type="submit"> <input type="reset">
     </form> 
