847a3e218a5bc8df9fbc3f91c7aff92570117118
EmmanuelPutra <72493099+EmmanuelPutra@users.noreply.github.com>
2021-06-08 22:10:21 +0700

    Update Main 2.0

--- application/views/main.php
+++ application/views/main.php
@@ -5,12 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootstrap.min.css">
-    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style.css"> -->
+    <title>Ocean.Id</title>
 </head>
-<body>
+<body class="p-3 mb-2 bg-info text-dark">
+    <!-- <div class="p-3 mb-2 bg-info text-dark">.bg-info</div> -->
     <center><br>
-    <h1>UNDER CONSTRUCTION</h1>
-    <img src="<?php echo base_url('img/main.jpg'); ?>" width="400px" height="400px">
-    <h1>UNDER CONSTRUCTION</h1>
+    <h3>OCEAN</h3><br>
+    <!-- <img src="<?php echo base_url('img/main.jpg'); ?>" width="50px" height="50px"><br> -->
+    <a href="#" data-toggle="popover" title="ㅤNila" data-content="Ikan nila adalah sejenis ikan konsumsi air tawar. Ikan ini diintroduksi dari Afrika, tepatnya Afrika bagian timur, pada tahun 1969, dan kini menjadi ikan peliharaan yang populer di kolam-kolam air tawar di Indonesia sekaligus hama di setiap sungai dan danau Indonesia."> <img src="<?php echo base_url('img/Nila.png'); ?>" width="150px" height="100px" align = "left" alt="Nila"></a><br>
+    <a href="#" data-toggle="popover" title="ㅤLele" data-content="Lele atau ikan keli, adalah sejenis ikan yang hidup di air tawar. Lele mudah dikenali karena tubuhnya yang licin, agak pipih memanjang, serta memiliki kumis yang panjang, yang mencuat dari sekitar bagian mulutnya."> <img src="<?php echo base_url('img/Lele.png'); ?>" width="150px" height="100px" align = "right" alt="Lele"><br>
+    <a href="#" data-toggle="popover" title="ㅤTongkol" data-content="Ikan tongkol berwarna agak gelap dan langsing, panjang ikan tongkol rata-rata sekitar 60 cm. Bagian punggung tongkol berwarna biru gelap metalik dengan pola garis-garis. Tongkol lebih banyak dikonsumsi masyarakat Jawa dan Sumatera."> <img src="<?php echo base_url('img/Tongkol.png'); ?>" width="150px" height="100px" align = "left" alt="Tongkol"><br>
+    <a href="#" data-toggle="popover" title="ㅤTuna" data-content="Tuna merupakan ikan laut pelagik yang termasuk bangsa Thunnini, terdiri dari beberapa spesies dari famili skombride, terutama genus Thunnus. Ikan ini adalah perenang andal. Tidak seperti kebanyakan ikan yang memiliki daging berwarna putih, daging tuna berwarna merah muda sampai merah tua."> <img src="<?php echo base_url('img/Tuna.png'); ?>" width="150px" height="100px" align = "right" alt="Tuna"><br>
+    <a href="#" data-toggle="popover" title="ㅤTeri" data-content="Ikan teri adalah sekelompok ikan laut kecil anggota suku Engraulidae. Nama ini mencakup berbagai ikan dengan warna tubuh perak kehijauan atau kebiruan. Walaupun anggota Engraulidae ada yang memiliki panjang maksimum 23 cm, nama ikan teri biasanya diberikan bagi ikan dengan panjang maksimum 5 cm."> <img src="<?php echo base_url('img/Teri.png'); ?>" width="150px" height="100px" align = "right" alt="Teri"><br>
+    <a href="#" data-toggle="popover" title="ㅤMakarel" data-content="Makerel atau makarel adalah sebutan bagi sekelompok ikan laut yang terdiri dari beberapa marga anggota famili Scombridae. Dalam peristilahan bahasa Inggris, sebutan mackerel juga mencakup kelompok ikan tenggiri dan kembung."> <img src="<?php echo base_url('img/Makarel.png'); ?>" width="150px" height="100px" align = "left" alt="Makarel"><br>
+    <a href="#" data-toggle="popover" title="ㅤTenggiri" data-content="Scomberomorini, umumnya disebut ikan tenggiri, adalah suku ikan bertulang pada keluarga makerel, Scombridae – yang masih memiliki kaitan dengan suku makerel, tuna dan bonito, ditambah Gasterochisma melampus. Suku ini terdiri dari 21 spesies dalam tiga genera: Acanthocybium A. solandri Grammatorcynus G. bicarinatus"> <img src="<?php echo base_url('img/Tenggiri.png'); ?>" width="150px" height="100px" align = "right" alt="Tenggiri"><br>
+    <a href="#" data-toggle="popover" title="ㅤWader" data-content="Wader bintik-dua adalah sejenis ikan kecil anggota suku Cyprinidae anak-suku Cyprininae. Ikan ini menyebar di Indonesia bagian barat, Indocina, dan Filipina. Nama-nama daerahnya di antaranya adalah beunteur, wader cakul atau wader pada umumnya, puyan, tanah atau sepadak dan lain-lain."> <img src="<?php echo base_url('img/Wader.png'); ?>" width="150px" height="100px" align = "right" alt="Wader"><br> 
+    <a href="#" data-toggle="popover" title="ㅤHiu" data-content="Ikan Hiu adalah sekelompok ikan dengan kerangka tulang rawan yang lengkap dan tubuh yang ramping. Mereka bernapas dengan menggunakan lima liang insang di samping, atau dimulai sedikit di belakang, kepalanya."> <img src="<?php echo base_url('img/Hiu.png'); ?>" width="150px" height="100px" align = "left" alt="Hiu"><br> 
+    <a href="#" data-toggle="popover" title="ㅤBelanak" data-content="Belanak adalah sejenis ikan laut tropis dan subtropis yang bentuknya hampir menyerupai bandeng. Dalam bahasa Inggris dikenal sebagai blue-spot mullet atau blue-tail mullet."> <img src="<?php echo base_url('img/Belanak.png'); ?>" width="150px" height="100px" align = "right" alt="Belanak"><br>
+    
+    <script>
+    $(document).ready(function(){
+        $('[data-toggle="popover"]').popover();   
+    });
+    </script>
+
 </body>
 </html>
