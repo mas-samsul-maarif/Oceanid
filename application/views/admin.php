7a2cd90421e1b64ce5020eeb29bce242800f214d
EmmanuelPutra <72493099+EmmanuelPutra@users.noreply.github.com>
2021-06-24 21:53:43 +0700

    Update admin.php

--- application/views/admin.php
+++ application/views/admin.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootstrap.min.css">
-    <title>Document</title>
+    <title>Admin</title>
 </head>
 <body>
     <br><center>
