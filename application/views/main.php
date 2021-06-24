c7082e8968a462752f5db5c9da1173d4e10ed12b
bachtiar22 <30924047+bachtiar22@users.noreply.github.com>
2021-06-24 20:50:59 +0700

    Add files via upload

--- application/views/main.php
+++ application/views/main.php
@@ -10,24 +10,114 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style.css"> -->
     <title>Ocean.Id</title>
+
+    <style type="text/css">
+
+    #backsea {
+        background: linear-gradient(#1cb5e0,#000046);   
+    }
+
+    span {
+        color: white;
+    }
+
+    @media only screen and (max-width: 576px) {
+         img {
+            width: 100px;
+            height: 50px;
+        }
+    }
+
+
+    </style>
+
 </head>
-<body class="p-3 mb-2 bg-info text-dark">
+<body class="p-3 mb-2" id="backsea">
     <!-- <div class="p-3 mb-2 bg-info text-dark">.bg-info</div> -->
     <center><br>
     <h3>OCEAN</h3><br>
     <!-- <img src="<?php echo base_url('img/main.jpg'); ?>" width="50px" height="50px"><br> -->
-    <a href="#" data-toggle="popover" title="ㅤNila" data-content="Ikan nila adalah sejenis ikan konsumsi air tawar. Ikan ini diintroduksi dari Afrika, tepatnya Afrika bagian timur, pada tahun 1969, dan kini menjadi ikan peliharaan yang populer di kolam-kolam air tawar di Indonesia sekaligus hama di setiap sungai dan danau Indonesia."> <img src="<?php echo base_url('img/nila.png'); ?>" width="150px" height="100px" align = "left" alt="Nila"></a><br>
-    <a href="#" data-toggle="popover" title="ㅤLele" data-content="Lele atau ikan keli, adalah sejenis ikan yang hidup di air tawar. Lele mudah dikenali karena tubuhnya yang licin, agak pipih memanjang, serta memiliki kumis yang panjang, yang mencuat dari sekitar bagian mulutnya."> <img src="<?php echo base_url('img/Lele.png'); ?>" width="150px" height="100px" align = "right" alt="Lele"><br>
-    <a href="#" data-toggle="popover" title="ㅤTongkol" data-content="Ikan tongkol berwarna agak gelap dan langsing, panjang ikan tongkol rata-rata sekitar 60 cm. Bagian punggung tongkol berwarna biru gelap metalik dengan pola garis-garis. Tongkol lebih banyak dikonsumsi masyarakat Jawa dan Sumatera."> <img src="<?php echo base_url('img/Tongkol.png'); ?>" width="150px" height="100px" align = "left" alt="Tongkol"><br>
-    <a href="#" data-toggle="popover" title="ㅤTuna" data-content="Tuna merupakan ikan laut pelagik yang termasuk bangsa Thunnini, terdiri dari beberapa spesies dari famili skombride, terutama genus Thunnus. Ikan ini adalah perenang andal. Tidak seperti kebanyakan ikan yang memiliki daging berwarna putih, daging tuna berwarna merah muda sampai merah tua."> <img src="<?php echo base_url('img/Tuna.png'); ?>" width="150px" height="100px" align = "right" alt="Tuna"><br>
-    <a href="#" data-toggle="popover" title="ㅤTeri" data-content="Ikan teri adalah sekelompok ikan laut kecil anggota suku Engraulidae. Nama ini mencakup berbagai ikan dengan warna tubuh perak kehijauan atau kebiruan. Walaupun anggota Engraulidae ada yang memiliki panjang maksimum 23 cm, nama ikan teri biasanya diberikan bagi ikan dengan panjang maksimum 5 cm."> <img src="<?php echo base_url('img/Teri.png'); ?>" width="150px" height="100px" align = "left" alt="Teri"><br>
-    <a href="#" data-toggle="popover" title="ㅤMakarel" data-content="Makerel atau makarel adalah sebutan bagi sekelompok ikan laut yang terdiri dari beberapa marga anggota famili Scombridae. Dalam peristilahan bahasa Inggris, sebutan mackerel juga mencakup kelompok ikan tenggiri dan kembung."> <img src="<?php echo base_url('img/Makarel.png'); ?>" width="150px" height="100px" align = "right" alt="Makarel"><br>
-    <a href="#" data-toggle="popover" title="ㅤTenggiri" data-content="Scomberomorini, umumnya disebut ikan tenggiri, adalah suku ikan bertulang pada keluarga makerel, Scombridae – yang masih memiliki kaitan dengan suku makerel, tuna dan bonito, ditambah Gasterochisma melampus. Suku ini terdiri dari 21 spesies dalam tiga genera: Acanthocybium A. solandri Grammatorcynus G. bicarinatus"> <img src="<?php echo base_url('img/Tenggiri.png'); ?>" width="150px" height="100px" align = "left" alt="Tenggiri"><br>
-    <a href="#" data-toggle="popover" title="ㅤWader" data-content="Wader bintik-dua adalah sejenis ikan kecil anggota suku Cyprinidae anak-suku Cyprininae. Ikan ini menyebar di Indonesia bagian barat, Indocina, dan Filipina. Nama-nama daerahnya di antaranya adalah beunteur, wader cakul atau wader pada umumnya, puyan, tanah atau sepadak dan lain-lain."> <img src="<?php echo base_url('img/Wader.png'); ?>" width="150px" height="100px" align = "right" alt="Wader"><br> 
-    <a href="#" data-toggle="popover" title="ㅤBelanak" data-content="Belanak adalah sejenis ikan laut tropis dan subtropis yang bentuknya hampir menyerupai bandeng. Dalam bahasa Inggris dikenal sebagai blue-spot mullet atau blue-tail mullet."> <img src="<?php echo base_url('img/Belanak.png'); ?>" width="150px" height="100px" align = "right" alt="Belanak"><br>
-    <a href="#" data-toggle="popover" title="ㅤHiu" data-content="Ikan Hiu adalah sekelompok ikan dengan kerangka tulang rawan yang lengkap dan tubuh yang ramping. Mereka bernapas dengan menggunakan lima liang insang di samping, atau dimulai sedikit di belakang, kepalanya."> <img src="<?php echo base_url('img/Hiu.png'); ?>" width="150px" height="100px" align = "right" alt="Hiu"><br>
-    <a href="#" data-toggle="popover" title="ㅤBarakuda" data-content="Ikan barakuda adalah ikan dalam kelas Actinopterygii yang dikenal berwujud menyeramkan dan berukuran tubuh besar, yaitu sampai panjang enam kaki dan lebar satu kaki."> <img src="<?php echo base_url('img/barakuda.png'); ?>" width="150px" height="100px" align = "right" alt="Hiu"><br> 
+    <div class="container">
+
+    <!-- Urutan ke 1-->
+    <div class="row" style="margin-bottom: 50px; margin-top: 50px;">
+        <div class="col-3 col-4"> 
+            <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤNila" data-content="Ikan nila adalah sejenis ikan konsumsi air tawar. Ikan ini diintroduksi dari Afrika, tepatnya Afrika bagian timur, pada tahun 1969, dan kini menjadi ikan peliharaan yang populer di kolam-kolam air tawar di Indonesia sekaligus hama di setiap sungai dan danau Indonesia."> <img src="<?php echo base_url('img/nila.png'); ?>" width="150px" height="100px" alt="Nila"></a><br><span><b>Nila</b></span>
+        </div>
+        <div class="col-3 col-4">
+             <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤLele" data-content="Lele atau ikan keli, adalah sejenis ikan yang hidup di air tawar. Lele mudah dikenali karena tubuhnya yang licin, agak pipih memanjang, serta memiliki kumis yang panjang, yang mencuat dari sekitar bagian mulutnya."> <img src="<?php echo base_url('img/Lele.png'); ?>" width="150px" height="100px" alt="Lele"></a><br><span><b>Lele</b></span>
+         </div>
+        <div class="col-3 col-4"> 
+            
+             <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤWader" data-content="Wader bintik-dua adalah sejenis ikan kecil anggota suku Cyprinidae anak-suku Cyprininae. Ikan ini menyebar di Indonesia bagian barat, Indocina, dan Filipina. Nama-nama daerahnya di antaranya adalah beunteur, wader cakul atau wader pada umumnya, puyan, tanah atau sepadak dan lain-lain."> <img src="<?php echo base_url('img/Wader.png'); ?>" width="150px" height="100px" alt="Wader"></a><br><span><b>Wader</b></span>
+        </div>
+        
+    </div>
+    <span >Kedalaman 1m - 3m </span>
+    <hr>
     
+<!-- Urutan ke 2-->
+   <div class="row" style="margin-bottom: 50px; margin-top: 50px;">
+       <div class="col-2 col-2"> 
+            
+        </div>
+        <div class="col-4 col-4">
+            <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤHiu" data-content="Ikan Hiu adalah sekelompok ikan dengan kerangka tulang rawan yang lengkap dan tubuh yang ramping. Mereka bernapas dengan menggunakan lima liang insang di samping, atau dimulai sedikit di belakang, kepalanya."> <img src="<?php echo base_url('img/Hiu.png'); ?>" width="150px" height="100px" alt="Hiu"></a><br><span><b>Hiu</b></span>
+         </div>
+        <div class="col-4 col-4"> 
+             <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤTeri" data-content="Ikan teri adalah sekelompok ikan laut kecil anggota suku Engraulidae. Nama ini mencakup berbagai ikan dengan warna tubuh perak kehijauan atau kebiruan. Walaupun anggota Engraulidae ada yang memiliki panjang maksimum 23 cm, nama ikan teri biasanya diberikan bagi ikan dengan panjang maksimum 5 cm."> <img src="<?php echo base_url('img/Teri.png'); ?>" width="150px" height="100px" alt="Teri"></a><br><span><b>Teri</b></span>
+        </div>
+        <div class="col-2 col-2"> 
+            
+        </div>
+   </div>
+  <span > Kedalaman 30m - 35m </span>
+    <hr>
+
+<!-- Urutan ke 3-->
+    <div class="row" style="margin-bottom: 50px; margin-top: 50px;">
+        <div class="col-4 col-4"> 
+            <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤTuna" data-content="Tuna merupakan ikan laut pelagik yang termasuk bangsa Thunnini, terdiri dari beberapa spesies dari famili skombride, terutama genus Thunnus. Ikan ini adalah perenang andal. Tidak seperti kebanyakan ikan yang memiliki daging berwarna putih, daging tuna berwarna merah muda sampai merah tua."> <img src="<?php echo base_url('img/Tuna.png'); ?>" width="150px" height="100px" alt="Tuna"></a><br><span><b>Tuna</b></span>
+        </div>
+        <div class="col-4 col-4">
+                <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤTenggiri" data-content="Scomberomorini, umumnya disebut ikan tenggiri, adalah suku ikan bertulang pada keluarga makerel, Scombridae – yang masih memiliki kaitan dengan suku makerel, tuna dan bonito, ditambah Gasterochisma melampus. Suku ini terdiri dari 21 spesies dalam tiga genera: Acanthocybium A. solandri Grammatorcynus G. bicarinatus"> <img src="<?php echo base_url('img/Tenggiri.png'); ?>" width="150px" height="100px" alt="Tenggiri"></a><br><span><b>Tenggiri</b></span>
+         </div>
+        <div class="col-4 col-4"> 
+              <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤTongkol" data-content="Ikan tongkol berwarna agak gelap dan langsing, panjang ikan tongkol rata-rata sekitar 60 cm. Bagian punggung tongkol berwarna biru gelap metalik dengan pola garis-garis. Tongkol lebih banyak dikonsumsi masyarakat Jawa dan Sumatera."> <img src="<?php echo base_url('img/Tongkol.png'); ?>" width="150px" height="100px" alt="Tongkol"></a><br><span><b>Tongkol</b></span>
+        
+        </div>
+        
+    </div>
+ <span > Kedalaman 40m - 100m </span>
+    <hr>
+
+<!-- Urutan ke 4-->
+     <div class="row" style="margin-bottom: 50px; margin-top: 50px;">
+       <div class="col-2 col-2"> 
+            
+        </div>
+        <div class="col-4 col-4"> 
+         <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤBelanak" data-content="Belanak adalah sejenis ikan laut tropis dan subtropis yang bentuknya hampir menyerupai bandeng. Dalam bahasa Inggris dikenal sebagai blue-spot mullet atau blue-tail mullet."> <img src="<?php echo base_url('img/Belanak.png'); ?>" width="150px" height="100px" alt="Belanak"></a><br><span><b>Belanak</b></span>
+         </div>
+        <div class="col-4 col-4"> 
+             <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤMakarel" data-content="Makerel atau makarel adalah sebutan bagi sekelompok ikan laut yang terdiri dari beberapa marga anggota famili Scombridae. Dalam peristilahan bahasa Inggris, sebutan mackerel juga mencakup kelompok ikan tenggiri dan kembung."> <img src="<?php echo base_url('img/Makarel.png'); ?>" width="150px" height="100px" alt="Makarel"></a><br><span><b>Makarel</b></span>
+        </div>
+        <div class="col-2 col-2"> 
+            
+        </div>
+   </div>
+<span > Kedalaman 120m - 1000m </span>
+    <hr>
+<!-- Urutan ke 5-->
+   <div class="row" style="margin-top: 50px;">
+       <div>
+         <a href="#" data-toggle="popover" data-trigger="focus" title="ㅤBarakuda" data-content="Ikan barakuda adalah ikan dalam kelas Actinopterygii yang dikenal berwujud menyeramkan dan berukuran tubuh besar, yaitu sampai panjang enam kaki dan lebar satu kaki."> <img src="<?php echo base_url('img/barakuda.png'); ?>" width="150px" height="100px" align = "center" alt="barakuda"></a><br><span><b>Barakuda</b></span>
+       </div>
+   </div>
+<span > Kedalaman 1000m+ </span>
+    <hr>
+    </div>
+
     <script>
     $(document).ready(function(){
         $('[data-toggle="popover"]').popover();   
